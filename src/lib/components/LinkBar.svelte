9927ad4bd6456f2fe43f66921bc9ca97cdde1269
InnoNodo <k.nosov@innopolis.university>
2024-07-18 14:54:33 +0300

    Add Tailwind CSS and change animation for about us page

--- src/lib/components/LinkBar.svelte
+++ src/lib/components/LinkBar.svelte
@@ -28,8 +28,8 @@
     .button {
         outline: 0 !important;
         border: 0 !important;
-        width: 20px;
-        height: 20px;
+        width: 30px;
+        height: 30px;
         border-radius: 50%;
         background-color: transparent;
         display: flex;
--- src/routes/authorization/+page.svelte
+++ src/routes/authorization/+page.svelte
@@ -2,19 +2,56 @@
     import Authorization from "$lib/components/Authorization.svelte";
 </script>
 
-<div class="authorization-block">
-    <!--Image-->
-    <Authorization />
-</div>
+<section>
+    <div id="main">
+        <div class="photo-block">
+            <img src="/assets/authorization/hands.png" alt="Hands" >
+        </div>
+
+        <div class="authorization-block">
+            <Authorization />
+        </div>
+    </div>
+</section>
 
 <style>
-    .authorization-block {
-        min-height: 36em;
+    #main {
         display: flex;
+        flex-direction: column;
         align-items: center;
         justify-content: center;
     }
+
+    .photo-block {
+        flex: 1;
+        display: flex;
+        justify-content: center;
+    }
+
+    img {
+        max-width: 65%;
+        height: auto;
+    }
+
+    .authorization-block {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex: 1;
+        width: 100%;
+    }
+
+    @media(min-width: 768px) {
+        #main {
+            flex-direction: row;
+        }
+
+        .photo-block {
+            max-width: 50%;
+        }
+
+        .authorization-block {
+            max-width: 50%;
+        }
+    }
 </style>
-
-
-
